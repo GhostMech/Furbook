875977a24de902343db240889a81f9b91c7ceb06
Gary Howard <garyhowardpctech@yahoo.com>
2017-07-17 17:26:23 -0400

    Added some very basic tests of my own.

---
+++ tests/Feature/GMHTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithoutMiddleware;
+use Illuminate\Foundation\Testing\DatabaseMigrations;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+
+class GMHTest extends TestCase
+{
+    /**
+     * A basic test example.
+     *
+     * @return void
+     */
+    public function testBasicTest()
+    {
+        $response = $this->get('/');
+
+        $response->assertStatus(200);
+    }
+    
+    public function testCatsPath()
+    {
+        $response = $this->get('/');
+        $response->assertViewIs('welcome');
+    }
+}
