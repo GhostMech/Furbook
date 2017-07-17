3c6bd7e2234ab8314c33d04d85c347142c311a46
Gary Howard <garyhowardpctech@yahoo.com>
2017-07-17 14:27:43 -0400

    Allow breed 'name' parameter to be fillable by mass assignment.

--- app/Breed.php
+++ app/Breed.php
@@ -9,6 +9,8 @@ class Breed extends Model
     // pg49 L5 Essentials
     public $timestamps = false;
     
+    protected $fillable = [ 'name'];
+    
     public function cats()
     {
         return $this->hasMany('App\Cat');
