07889b447aadf76fc56e3a4f8280535bd4a2d2ea
Gary Howard <garyhowardpctech@yahoo.com>
2017-07-17 17:25:40 -0400

    Added IF conditionals for "breeds" and "breed" pages. Changed "web.php" to reflect this.

--- resources/views/breed.blade.php
+++ resources/views/breed.blade.php
@@ -7,7 +7,7 @@
             <h2 class="text text-danger">{{ $breedname }}</h2>
         @else
             <h2 class="text text-success">{{ $breedname }}</h2>
-            @if (count($cats) > 0)
+            @if ($catsCount > 0)
                 @foreach ($cats as $cat)
                     <p><a href="/cats/{{ $cat->id }}">{{ $cat->name }}</a></p>
                 @endforeach
--- resources/views/breeds.blade.php
+++ resources/views/breeds.blade.php
@@ -3,8 +3,12 @@
         @include('partials.header')
     @stop
     @section('content')
-        <h2>Cat Breeds</h2>  
-        @foreach ($breeds as $breed)
-            <p><a href="/cats/breed/{{ strtolower($breed->name) }}">{{ $breed->name }}</a></p>
-        @endforeach
+        <h2>Cat Breeds</h2>
+        @if (count($breeds) > 0)
+            @foreach ($breeds as $breed)
+                <p><a href="/cats/breed/{{ strtolower($breed->name) }}">{{ $breed->name }}</a></p>
+            @endforeach
+        @else
+            <p>There are no breeds.</p>
+        @endif
     @stop
