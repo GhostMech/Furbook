12a02a49587c1c06709e70f2b2120022de612cb0
Gary Howard <garyhowardpctech@yahoo.com>
2017-07-31 16:11:08 -0400

    Added breed link to each cats' description.

--- resources/views/cat.blade.php
+++ resources/views/cat.blade.php
@@ -1,4 +1,5 @@
 @extends('layouts.cat_master')
     @section('content')
-        {{ $cat->name }}
+        <strong>{{ $cat->name }}'s</strong> breed is <a href="/cats/breed/{{ $cat->breed->name }}">{{ $cat->breed->name }}</a>, 
+        and this cat was born on {{ $cat->date_of_birth }}.
     @stop
