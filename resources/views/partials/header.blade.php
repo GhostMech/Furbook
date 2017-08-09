b6413b73da071e4e1d948880472d6fc098b1a8b8
Gary Howard <garyhowardpctech@yahoo.com>
2017-08-09 15:13:23 -0400

    Changed $_SERVER['REQUEST_URI'] IF statements

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,13 +1,13 @@
 <div class="container">
     <div class="row">
         <div class="col-xs-6">
-            <span class="h4">Welcome to <h1>Furbook!</h1>
+            <span class="h4">Welcome to </span><h1>Furbook!</h1>
         </div>
         <div class="col-xs-6">
-        @if ($_SERVER['PHP_SELF'] !== '/index.php/cats')
+        @if ($_SERVER['REQUEST_URI'] !== '/cats')
         <span class="pull-right"><a href="/cats"><h4>Return to Homepage</h4></a>
             <?php $isBreed = explode('/', $_SERVER['REQUEST_URI']); ?>
-            @if ($isBreed[2] === 'breed')
+            @if ($_SERVER['REQUEST_URI'] !== '/cats/breeds')
                 <span class="pull-right"><a href="/cats/breeds"><h5>Return to Cat Breeds</h5></a></span>
             @endif
         @endif
